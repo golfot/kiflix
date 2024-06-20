d7d580eda4d7a4441f8ad57e3a875825ae7500e6
Golfot <rizkifundie@gmail.com>
2024-06-21 06:47:39 +0700

    Update terkait.js

--- api/terkait.js
+++ api/terkait.js
@@ -27,7 +27,7 @@ module.exports = async (req, res) => {
     }
 
    
-    let url = `${targetUrl}${id}/`;
+    let url = `${targetUrl}${slug}/`;
 
     https.get(url, (response) => {
         let data = '';

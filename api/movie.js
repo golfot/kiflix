af46be2b0a80420ef906a563a0e9dc96558f1928
Golfot <rizkifundie@gmail.com>
2024-08-06 13:28:11 +0700

    Update movie.js

--- api/movie.js
+++ api/movie.js
@@ -1,120 +1,37 @@
-const https = require('https');
-const jsdom = require('jsdom');
-const { JSDOM } = jsdom;
-const targetUrl = require('./targeturl');
+const fetch = require('node-fetch');
+
+const API_URL = 'https://chatgpt.org.ua/api/?action=check_answer';
+const HEADERS = {
+  'Content-Type': 'application/x-www-form-urlencoded',
+  'User-Agent': 'Mozilla/5.0 (Linux; Android 10; Redmi Note 7 Build/QKQ1.190910.002) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.6478.188 Mobile Safari/537.36'
+};
 
 module.exports = async (req, res) => {
-    // Menambahkan header CORS ke dalam respons
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
-    // Mengatasi preflight request (OPTIONS)
-    if (req.method === 'OPTIONS') {
-        res.status(200).end();
-        return;
-    }
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
 
-    // Mengambil nilai parameter slugs dan server dari permintaan
-    const slug = req.query.slug || '';
+  try {
+    const prompt = req.query.cek || '';
+     const body = `question_hash=${encodeURIComponent(prompt)}&bfp_hash=277174405&lang=en`;
 
-
-    // Memeriksa apakah parameter slugs dan server telah diberikan
-    if (!slug) {
-        res.status(400).json({ error: 'Parameter slug movies tidak ditemukan' });
-        return;
-    }
-
-    let url = `${targetUrl}${slug}/`;
-
-    https.get(url, (response) => {
-        let data = '';
-
-        // Mengumpulkan data yang diterima
-        response.on('data', (chunk) => {
-            data += chunk;
-        });
-
-        // Proses data setelah selesai diterima
-        response.on('end', () => {
-            const dom = new JSDOM(data);
-            const document = dom.window.document;
-
-            // Mengambil data simponis
-            const simpinisElement = document.querySelector('div[itemprop="description"] p');
-            const simponis = simpinisElement ? simpinisElement.textContent.trim() : 'N/A';
-
-            // Mengambil semua data genre
-            const genreElements = document.querySelectorAll('div[class="sgeneros"] a');
-            let genre = [];
-
-            genreElements.forEach(element => {
-                    const title = element.textContent.trim();
-      
-                    let slug = element.getAttribute('href').trim();
-                    // Menghapus bagian "https" dan domain dari slug menggunakan regex
-                    slug = slug.replace(/^https?:\/\/[^/]+/, '');
-                    // Menghapus simbol slash ('/') pertama dan terakhir dari slug
-                    slug = slug.replace(/^\/|\/$/g, '');
-                    genre.push({
-                        title: title,
-                        slug: slug
-                    });
-                
-            });
-
-            const castElements = document.querySelectorAll('div[class="person"]');
-            let cast = [];
-
-            castElements.forEach(element => {
-                    const caracter = element.querySelector('div.caracter') ? element.querySelector('div.caracter').textContent.trim() : 'N/A';
-                    const thumb = element.querySelector('img') ? element.querySelector('img').getAttribute('src') : 'N/A';
-                    const title = element.querySelector('img') ? element.querySelector('img').getAttribute('alt') : 'N/A';
-               
-
-                
-                    let slug = element.querySelector('div.name a') ? element.querySelector('div.name a').getAttribute('href') : 'N/A';
-                    // Menghapus bagian "https" dan domain dari slug menggunakan regex
-                    slug = slug.replace(/^https?:\/\/[^/]+/, '');
-                    // Menghapus simbol slash ('/') pertama dan terakhir dari slug
-                    slug = slug.replace(/^\/|\/$/g, '');
-                    cast.push({
-                        title: title,
-                        caracter: caracter,
-                        thumb: thumb,
-                        slug: slug
-                    });
-                
-            });
-
-            // Mengambil durasi
-            const iframeElement = document.querySelector('span[class="runtime"]');
-            const durasi = iframeElement ? iframeElement.textContent.trim() : 'N/A';
-
-            // Mengambil Poster
-            const posterElement = document.querySelector('div[class="poster"] img[itemprop="image"]');
-            const poster = posterElement ? posterElement.getAttribute('src') : 'N/A';
-
-
-            // Mengambil title
-            const titleElement = document.querySelector('h1');
-            const title = titleElement ? titleElement.textContent.trim() : 'N/A';
-
-
-            // Membuat objek detail movie
-            const detailMovieObject = {
-                title,
-                poster,
-                simponis,
-                durasi,
-                cast,
-                genre
-            };
-
-            res.status(200).json(detailMovieObject);
-        });
-
-    }).on('error', (err) => {
-        res.status(500).json({ error: err.message });
+    const response = await fetch(API_URL, {
+      method: 'POST',
+      headers: HEADERS,
+      body
     });
+
+    const data = await response.json();
+    const replyData = data.answer_text || 'Bot tidak bisa mengecek pesan ini';
+
+    res.status(200).send(replyData);
+
+  } catch (error) {
+    res.status(500).send({ error: 'An error occurred' });
+  }
 };

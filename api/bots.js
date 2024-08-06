e9b867b432deba2035d4a25d582376e1f3935f08
Golfot <rizkifundie@gmail.com>
2024-08-06 14:04:30 +0700

    Update and rename series.js to bots.js

---
+++ api/bots.js
@@ -0,0 +1,39 @@
+const fetch = require('node-fetch');
+
+const PESAN_API_URL = 'https://kiflix.vercel.app/api/pesan';
+const CEK_PESAN_API_URL = 'https://kiflix.vercel.app/api/cekpesan';
+
+module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
+  if (req.method === 'OPTIONS') {
+    res.status(200).end();
+    return;
+  }
+
+  if (req.method === 'POST') {
+    try {
+      const isipesan = req.body.isipesan || '';
+
+      // Kirim pesan
+      const pesanResponse = await fetch(`${PESAN_API_URL}?isipesan=${encodeURIComponent(isipesan)}`);
+      const pesanText = await pesanResponse.text(); // Mengambil respons sebagai teks
+
+      // Cek pesan
+      const cekResponse = await fetch(`${CEK_PESAN_API_URL}?cek=${pesanText}`);
+      const cekhasilnya = await cekResponse.text(); // Mengambil respons sebagai text juga
+      
+      // Mengakses teks dari JSON
+      const cekText = cekhasilnya.text || 'No text found';
+
+      res.status(200).send(cekText);
+
+    } catch (error) {
+      res.status(500).json({ error: 'An error occurred' });
+    }
+  } else {
+    res.status(405).json({ error: 'Method not allowed' });
+  }
+};

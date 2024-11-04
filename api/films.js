716ab1a22688258e948faf24ee37db40ba7a1697
Golfot <rizkifundie@gmail.com>
2024-11-04 22:17:17 +0700

    Update and rename terkait.js to films.js

--- api/films.js
+++ api/films.js
@@ -15,19 +15,26 @@ module.exports = async (req, res) => {
         return;
     }
     
-    
-    // Mengambil nilai parameter slugs dan server dari permintaan
+    // Mengambil nilai parameter categori, country, dan genre dari permintaan
     const categori = req.query.categori || '';
+    const country = req.query.country || '';
+    const genre = req.query.genre || '';
 
-
-    // Memeriksa apakah parameter slugs dan server telah diberikan
-    if (!categori) {
-        res.status(400).json({ error: 'Parameter kategori tidak ditemukan' });
+    // Memeriksa apakah salah satu dari parameter telah diberikan
+    if (!categori && !country && !genre) {
+        res.status(400).json({ error: 'Parameter kategori, country, atau genre tidak ditemukan' });
         return;
     }
 
-   
-    let url = `${targetUrl}category/${categori}/`;
+    // Tentukan URL berdasarkan parameter yang diberikan
+    let url = '';
+    if (categori) {
+        url = `${targetUrl}category/${categori}/`;
+    } else if (country) {
+        url = `${targetUrl}country/${country}/`;
+    } else if (genre) {
+        url = `${targetUrl}genre/${genre}/`;
+    }
 
     https.get(url, (response) => {
         let data = '';
@@ -49,25 +56,29 @@ module.exports = async (req, res) => {
                 const poster = article.querySelector('img') ? article.querySelector('img').getAttribute('src') : 'N/A';
                 const title = article.querySelector('h2') ? article.querySelector('h2').textContent.trim() : 'N/A';
                 const tailer = article.querySelector('article div.gmr-popup-button a') ? article.querySelector('article div.gmr-popup-button a').getAttribute('href') : 'N/A';
-               
+
                 let slug = article.querySelector('div.item-article h2 a') ? article.querySelector('div.item-article h2 a').getAttribute('href') : 'N/A';
 
                 const type = slug.includes('/tv/') ? 'tv' : 'movie';
-                
+
+                // Mengatur nilai episode
+                const episode = type === 'tv' 
+                    ? (article.querySelector('div.gmr-numbeps') ? article.querySelector('div.gmr-numbeps').textContent.trim() : 'N/A')
+                    : 'N/A';
+
                 // Menghapus bagian "https" dan domain dari slug menggunakan regex
                 slug = slug.replace(/^https?:\/\/[^/]+/, '');
                 
                 // Menghapus simbol slash ('/') pertama dan terakhir dari slug
                 slug = slug.replace(/^\/|\/$/g, '');
                 
-
-                
                 results.push({
                     poster,
                     title,
                     tailer,
                     slug,
-                    type
+                    type,
+                    episode
                 });
             });
 
